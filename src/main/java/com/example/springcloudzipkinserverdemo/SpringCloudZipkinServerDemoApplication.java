14a8820a7cc1049b14cef491ff0e2e31f9db0985
mrzhu <2692027659@qq.com>
2018-10-30 17:21:14 +0800

    sleuth + zipkin + stream, binder + kafka : dependency

--- src/main/java/com/example/springcloudzipkinserverdemo/SpringCloudZipkinServerDemoApplication.java
+++ src/main/java/com/example/springcloudzipkinserverdemo/SpringCloudZipkinServerDemoApplication.java
@@ -2,10 +2,12 @@ package com.example.springcloudzipkinserverdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 import zipkin.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
-@EnableZipkinServer
+//@EnableZipkinServer
+@EnableZipkinStreamServer
 public class SpringCloudZipkinServerDemoApplication {
 
 	public static void main(String[] args) {
